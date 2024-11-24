e24a1a112b95b55a3649973549984f083205d018
Shiwani Shah <shahshiwani246@gmail.com>
2024-11-25 01:06:44 +0545

    Add project files.

---
+++ MoneyTransferApplication/Entities/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoneyTransferApplication.Entities
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
